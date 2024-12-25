a8fee904452ef7e27dcf3e7e2addc99b7844acd6
Abhi Reddy <anagireddygari@bowdoin.edu>
2024-12-25 09:51:54 -0800

    Inital Commit: Created webpage

---
+++ music-recomender/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Your Name. Inspired by Spotify.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ music-recomender/src/components/Recommender.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Recommender.css";
+
+function Recommender() {
+  return (
+    <section className="Recommender">
+      <h2>Discover Your Next Favorite Song</h2>
+      <button>Get Recommendations</button>
+    </section>
+  );
+}
+
+export default Recommender;
